71f2858da252a84d99ed38d1caf9ef81a8321218
Leo <lrmaldo@gmail.com>
2017-01-17 09:40:13 -0600

    creacion de cardview(listo) y fragment

---
+++ app/src/main/java/lrmaldo/platzigram/view/ContainerActivity.java
@@ -0,0 +1,15 @@
+package lrmaldo.platzigram.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import lrmaldo.platzigram.R;
+
+public class ContainerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_container);
+    }
+}
