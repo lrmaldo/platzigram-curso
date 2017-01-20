b81df61b798612d6c265d5854ad65ddae2e29858
Leo <lrmaldo@gmail.com>
2017-01-19 22:04:15 -0600

    agregacion de recyclerview

---
+++ app/src/main/java/lrmaldo/platzigram/adapter/PictureAdapterRecyclerview.java
@@ -0,0 +1,74 @@
+package lrmaldo.platzigram.adapter;
+
+import android.app.Activity;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import lrmaldo.platzigram.R;
+import lrmaldo.platzigram.model.Picture;
+
+/**
+ * Created by Leo on 18/01/2017.
+ */
+
+public class PictureAdapterRecyclerview extends RecyclerView.Adapter<PictureAdapterRecyclerview.PictureViewHolder>{
+
+    private ArrayList<Picture> pictures;
+    private int resource;
+    private Activity activity;
+
+    public PictureAdapterRecyclerview(ArrayList<Picture> pictures, int resource, Activity activity) {
+        this.pictures = pictures;
+        this.resource = resource;
+        this.activity = activity;
+    }
+
+    @Override
+    public PictureViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+       View view = LayoutInflater.from(parent.getContext()).inflate(resource,parent,false);
+
+
+        return new PictureViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(PictureViewHolder holder, int position) {
+        Picture picture = pictures.get(position);
+        holder.usernameCard.setText(picture.getUsername());
+        holder.timeCard.setText(picture.getTime());
+        holder.likenumberCard.setText(picture.getLikeNumber());
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return pictures.size();
+    }
+
+
+    public class PictureViewHolder extends RecyclerView.ViewHolder{
+        private ImageView pictureCard;
+        private TextView usernameCard;
+        private TextView timeCard;
+        private  TextView likenumberCard;
+
+        public PictureViewHolder(View itemView) {
+            super(itemView);
+        pictureCard     = (ImageView) itemView.findViewById(R.id.pictureCard);
+        usernameCard    = (TextView) itemView.findViewById(R.id.usernameCardview);
+        timeCard        = (TextView) itemView.findViewById(R.id.TimeCard);
+        likenumberCard  = (TextView) itemView.findViewById(R.id.likeNumberCard);
+
+
+        }
+
+    }
+}
